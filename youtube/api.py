b8ae3a804adb87aadff9d97cb3b2112be69d6ef1
Hepheir <hepheir@gmail.com>
2026-02-18 03:34:40 +0900

    feat(api)!: 댓글 수집용 메소드를 `get_threads()`, `get_comments()` 로 단순화

--- youtube/api.py
+++ youtube/api.py
@@ -3,7 +3,7 @@ from typing import Generator, Optional
 from youtube.models import *
 
 
-class BaseYoutubeAPI:
+class YoutubeAPI:
     def __init__(self, api_key: str, quota: int = 10000):
         # Do lazy-loading
         from googleapiclient.discovery import build
@@ -12,77 +12,40 @@ class BaseYoutubeAPI:
 
     def get_video(self, video_id: str) -> Video:
         """영상 ID를 통해 영상에 대한 기본적인 정보를 가져온다."""
-        return self._get_video_list_response(video_id).items[0]
-
-    def get_comment_threads_by_video(self, video_id: str) -> Generator[CommentThread, None, None]:
-        page_token = None
-        while True:
-            comment_thread_list = self._get_comment_threads_list_response_by_video(
-                video_id, page_token)
-            yield from comment_thread_list.items
-            page_token = comment_thread_list.nextPageToken
-            if not page_token:
-                break
-
-    def get_comment_threads_by_parent_comment(self, parent_comment_id: str) -> Generator[CommentThread, None, None]:
-        page_token = None
-        while True:
-            comment_thread_list = self._get_comment_threads_list_response_by_parent_comment(
-                parent_comment_id, page_token)
-            yield from comment_thread_list.items
-            page_token = comment_thread_list.nextPageToken
-            if not page_token:
-                break
-
-    def _get_video_list_response(self, video_id: str) -> VideoListResponse:
         request = self.client.videos().list(
             part="snippet",
             id=video_id,
         )
         self.quota -= 1
-        data = request.execute()
-        return VideoListResponse.from_dict(data)
+        response = VideoListResponse.from_dict(request.execute())
+        return response.items[0]
 
-    def _get_comment_threads_list_response_by_video(self, video_id: str, page_token: Optional[str]) -> CommentThreadListResponse:
-        request = self.client.commentThreads().list(
-            part="snippet,replies",
-            videoId=video_id,
-            pageToken=page_token,
-            maxResults=100,
-        )
-        self.quota -= 1
-        data = request.execute()
-        return CommentThreadListResponse.from_dict(data)
+    def get_threads(self, video_id: str) -> Generator[CommentThread, None, None]:
+        page_token = None
+        while True:
+            request = self.client.commentThreads().list(
+                part="snippet,replies",
+                videoId=video_id,
+                pageToken=page_token,
+                maxResults=100,
+            )
+            self.quota -= 1
+            response = CommentThreadListResponse.from_dict(request.execute())
+            yield from response.items
+            if not (page_token := response.nextPageToken):
+                break
 
-    def _get_comment_threads_list_response_by_parent_comment(self, parent_comment_id: str, page_token: Optional[str]) -> CommentThreadListResponse:
-        request = self.client.commentThreads().list(
-            part="snippet",
-            id=parent_comment_id,
-            pageToken=page_token,
-            maxResults=100,
-        )
-        self.quota -= 1
-        data = request.execute()
-        return CommentThreadListResponse.from_dict(data)
-
-
-class YoutubeAPI(BaseYoutubeAPI):
-    def get_comment_threads(self, video_id: str) -> Generator[CommentThread, None, None]:
-        """특정 동영상 안에 있는 모든 댓글 스레드를 불러온다."""
-        for thread in self.get_comment_threads_by_video(video_id=video_id):
-            yield from self._get_comment_thread_recursive(thread)
-
-    def _get_comment_thread_recursive(self, thread: CommentThread) -> Generator[CommentThread, None, None]:
-        yield thread
-        if thread.snippet.totalReplyCount == 0:
-            return
-        for sub_thread in self.get_comment_threads_by_parent_comment(thread.snippet.topLevelComment.id):
-            if thread.snippet.topLevelComment.id != sub_thread.snippet.topLevelComment.id:
-                yield from self._get_comment_thread_recursive(sub_thread)
-            else:
-                yield sub_thread
-
-    def get_comments(self, video_id: str) -> Generator[Comment, None, None]:
-        """특정 동영상 안에 있는 모든 댓글을 불러온다."""
-        for thread in self.get_comment_threads_by_video(video_id=video_id):
-            yield thread.snippet.topLevelComment
+    def get_comments(self, thread_id: str) -> Generator[Comment, None, None]:
+        page_token = None
+        while True:
+            request = self.client.comments().list(
+                part="id,snippet",
+                parentId=thread_id,
+                pageToken=page_token,
+                maxResults=100,
+            )
+            self.quota -= 1
+            response = CommentListResponse.from_dict(request.execute())
+            yield from response.items
+            if not (page_token := response.nextPageToken):
+                break
